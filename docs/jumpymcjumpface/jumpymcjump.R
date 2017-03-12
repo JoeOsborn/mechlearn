ee036cb0cbd06c3a57ca3cbe3a30c48fadca5e97
Christoffer Holmgard <holmgard@gmail.com>
2017-03-13 00:28:22 +0100

    Reducing to 1 dataset instead of 3

--- docs/jumpymcjumpface/jumpymcjump.R
+++ docs/jumpymcjumpface/jumpymcjump.R
@@ -23,7 +23,7 @@ extract <- read_delim("~/DevProjects/jupyter/mechlearn/docs/jumpymcjumpface/HAs.
                                                                 `up-control_gravity` = col_number(), 
                                                                 `up-control_reset` = col_number()), 
                   trim_ws = TRUE)
-View(extract)
+#View(extract)
 
 extract <- data.frame(extract)
 
@@ -33,54 +33,57 @@ extract$ground_mult = NULL
 extract$ground_gravity = NULL
 
 # Get relevant variables for analysis
-jumpdata <- extract[,c("up.fixed_reset","up.fixed_mult","up.control_reset","down_reset","down_mult","down_gravity","ground_reset","ground_gravity","minHoldDuration","maxHoldDuration")]
+#jumpdata <- extract[,c("down_reset","down_mult","down_gravity","up.fixed_reset","up.fixed_mult","up.fixed_gravity","ground_reset","ground_gravity","minHoldDuration","maxHoldDuration")]
+jumpdata <- extract[,c("down_reset","down_mult","down_gravity","up.fixed_reset","up.fixed_mult","up.fixed_gravity","minHoldDuration","maxHoldDuration")]
 temp <- extract$up.control_gravity
 temp[which(is.na(temp))] <- extract$up.fixed_gravity[which(is.na(temp))]
 jumpdata <- cbind(jumpdata,temp)
 names(jumpdata)[ncol(jumpdata)] <- "up.gravity_combined"
 rownames(jumpdata) <- extract$name
 
-
-# Identify three different types for PCA
-#extract <- cbind(extract,NA)
-#names(extract)[ncol(extract)] <- c("jump_type")
-#extract[which(!is.na(extract$up.control_reset)),]$jump_type = "Full"
-#extract[which(extract$minHoldDuration == extract$maxHoldDuration),]$jump_type = "Short"
-#full <- extract[which(extract$jump_type == "Full"),]
-#short <- extract[which(extract$jump_type == "Short"),]
-#short$up.control_reset = NULL
-#short$up.control_gravity = NULL
-#short$up.fixed_mult = NULL
-#shared <- extract[,!apply(is.na(extract), 2, any)]
-
 #----------------------------------------------
 # Visualizations for exploring the data - commented out, we don't need this right now
 #----------------------------------------------
 #Lattice plot of all features in summary statistics
-#splom(extract[5:ncol(extract)],main="All games")
-
-#splom(full[5:ncol(full)],main="Full jumps")
-#splom(short[,!apply(is.na(short), 2, any)],main="Short jumps")
-#splom(shared[5:ncol(shared)],main="Shared features")
+splom(jumpdata,main="All game characters")
 
 #----------------------------------------------
-# Unrotated PCA for all 3 kinds
+# Unrotated PCA jumpdata
 #----------------------------------------------
 #Principal component analysis
-#3 different jump types, short, full, and one with variables shared between all jumps
 
 jump_pcaFit <- PCA(X = jumpdata,ncol(jumpdata),scale.unit = TRUE)
+summary(jump_pcaFit)
+plot(jump_pcaFit$eig[,2],type="b", main = "Screeplot", xlab = "Component", ylab="Variance explained")
+xtable(jump_pcaFit$eig)
 
+# Print the contributions of the first 3 dimensions from PCA
+xtable(as.data.frame(jump_pcaFit$var$contrib))
 
-#full_pcaFit <- PCA(X = full[10:ncol(full)-1],ncol(full)-10,scale.unit = TRUE)
-#summary(full_pcaFit)
-#plot(full_pcaFit$eig[,2],type="b", main = "Full, screeplot", xlab = "Component", ylab="Variance explained")
+#----------------------------------------------
+# kMeans clustering
+#----------------------------------------------
+#K-means clustering
+wss <- (nrow(jumpdata*sum(apply(jumpdata,2,var))))
+for (i in 2:15){
+  wss[i] <- sum(kmeans(jumpdata, centers=i)$withinss)
+}
+rm(i)
+plot(1:15, wss, type="b", xlab="Number of kMeans Clusters", ylab="Within groups sum of squares")
+rm(wss)
 
-#short_pcaFit <- PCA(X = short[10:ncol(short)-1],ncol(short)-10,scale.unit = TRUE)
-#summary(short_pcaFit)
-#plot(short_pcaFit$eig[,2],type="b", main = "Short, screeplot", xlab = "Component", ylab="Variance explained")
+# K-Means Cluster Analysis
+jump_kMeansFit <- kmeans(jumpdata, 3) # 3 cluster solution
+#shared_kMedioidsFit <-  pamk(shared[10:ncol(shared)-1])
+#clusplot(shared[10:ncol(shared)-1], shared_kMeansFit$cluster, color=TRUE, shade=TRUE, labels=1, lines=0, main="Shared, kMeans clusters")
+fviz_cluster(jump_kMeansFit,data = jumpdata, geom = "text", repel = TRUE)
 
-#row.names(shared) <- shared$name
-#shared_pcaFit <- PCA(X = shared[10:ncol(shared)-1],ncol(shared)-10,scale.unit = TRUE)
-#summary(shared_pcaFit)
-#plot(shared_pcaFit$eig[,2],type="b", main = "Screeplot of Principal Components, shared features", xlab = "Component", ylab="Variance explained")
+# get cluster means
+aggregate(shared[10:ncol(shared)-1],by=list(shared_kMeansFit$cluster),FUN=mean)
+
+# append cluster assignment to each game
+shared_kMeans_cluster <- shared_kMeansFit$cluster
+table(shared_kMeans_cluster)
+shared <- data.frame(shared, shared_kMeans_cluster)
+shared_kMeans_profile <- shared_kMeansFit$centers
+xtable(table(shared$Developer,shared$shared_kMeans_cluster))
