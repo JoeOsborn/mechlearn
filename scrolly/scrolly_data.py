79aa8e5c90bf3bbd8460d9cfe335501995d33a1c
Joseph C. Osborn <jcoa2018@pomona.edu>
2019-03-29 13:47:39 -0700

    WIP scrolly, probably a dead end

--- scrolly/scrolly_data.py
+++ scrolly/scrolly_data.py
@@ -1,4 +1,4 @@
-import remocon
+import retro
 import itertools
 from PIL import Image
 import os
@@ -6,79 +6,35 @@ import csv
 
 # read playfile
 fm2file = "lordtom_tompa-smb3-100.fm2"
-info = remocon.read_fm2("../plays/" + fm2file)
-romname = info["romFilename"][0] + ".nes"
-controls_fm = info["player_controls"]
-controls = list(map(remocon.moves_to_bytes, controls_fm))
-pad = 10
-for p in range(pad):
-    for c in controls:
-        c.insert(0, 0)
-skip = 0
-for ci in range(len(controls)):
-    controls[ci] = controls[ci][skip:]
-tmax = min(len(controls[0]), 700)
-print("Load ../roms/{}".format(romname))
-remo = remocon.Mesen(
-    "../mesen/remocon/obj.x64/remocon",
-    "../roms/" + romname)
+os.chdir("../plays")
+movie = retro.Movie(fm2file)
+romname = "SuperMarioBros3-Nes"
+# TODO doesn't work for fm2 files, hack around it for now
+print(movie.get_game())
+os.chdir("../roms")
+env = retro.make(game=romname, state=None, use_restricted_actions=retro.Actions.ALL, players=movie.players)
+os.chdir("../scrolly")
+env.initial_state = movie.get_state()
+env.reset()
 
 os.makedirs("../data/{}/{}".format(romname, fm2file), exist_ok=True)
 
-remo.step([controls[0][0:tmax - 1]], remocon.Infos())
-frames = remo.step([controls[0][tmax - 1:tmax]], remocon.Infos(framebuffer=True))[0]
-# for t, frame in enumerate(frames):
-# fb = frame.framebuffer
-Image.fromarray(frames[0].framebuffer).save("../data/{}/{}/t_{}.png".format(romname, fm2file, tmax - 1))
+tmax = 3000
+t = 0
+scrolls = []
+while movie.step():
+    t += 1
+    if tmax > 0 and t > tmax:
+        break
+    keys = []
+    for p in range(movie.players):
+        for i in range(env.num_buttons):
+            keys.append(movie.get_key(i, p))
+    print("Upto", t)
+    obs, _rew, _done, info = env.step(keys)
+    Image.fromarray(obs).save("../data/{}/{}/t_{}.png".format(romname, fm2file, t))
+    scrolls.append((t, info["sx"], info["sy"]))
 
-# def scroll_mode(tiles_by_pixel):
-#     scrolls = {}
-#     mode = None
-#     mode_count = 0
-#     for row in tiles_by_pixel:
-#         for pix in row:
-#             if pix.x_scroll != 0 or pix.y_scroll != 0:
-#                 print(t, pix.x_scroll, pix.y_scroll)
-#             xsc = int(pix.x_scroll)
-#             ysc = int(pix.y_scroll)
-#             key = (xsc, ysc)
-#             scrolls[key] = scrolls.get(key, 0) + 1
-#             if scrolls[key] > mode_count:
-#                 mode = key
-#                 mode_count = scrolls[key]
-#     return mode
-
-
-# tskip = 500 + (hash(fm2file) % 100)
-# remo.step([controls[0][0:tskip - 1]], remocon.Infos())
-# tsteps = [11, 5, 7, 19, 3, 15, 1, 13, 23]
-# stepper = itertools.cycle(tsteps)
-# t = tskip
-# frame0 = remo.step(
-#     [controls[0][tskip - 1:tskip]],
-#     remocon.Infos(framebuffer=True,
-#                   tiles_by_pixel=True))[0][0]
-# fb0 = frame0.framebuffer
-# scroll0 = scroll_mode(frame0.tiles_by_pixel)
-# scrolls = [(t, 0, 0)]
-# while t < tmax:
-#     t0 = t
-#     tstep = next(stepper)
-#     t += tstep
-#     if tstep > 1:
-#         remo.step([controls[0][t0:t - 1]], remocon.Infos())
-#     frame = remo.step([controls[0][t - 1:t]], remocon.Infos(framebuffer=True, tiles_by_pixel=True))[0][0]
-#     fb = frame.framebuffer
-#     scroll = scroll_mode(frame.tiles_by_pixel)
-
-#     # write out fb for t
-#     Image.fromarray(fb).save("../data/{}/{}/t_{}.png".format(romname, fm2file, t))
-#     # add t, scrolldelta to csv
-#     scrolls.append((t, scroll[0] - scroll0[0], scroll[1] - scroll0[1]))
-
-#     fb0 = fb
-#     scroll0 = scroll
-
-# with open("../data/{}/{}/scrolls.csv".format(romname, fm2file), 'w') as csvfile:
-#     writer = csv.writer(csvfile)
-#     writer.writerows(scrolls)
+with open("../data/{}/{}/scrolls.csv".format(romname, fm2file), 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(scrolls)
